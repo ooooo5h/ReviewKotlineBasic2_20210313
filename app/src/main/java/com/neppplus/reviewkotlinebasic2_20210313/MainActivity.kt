84b941f6d248c4065d1160900662019123f927ee
전은형 <ooooo5h91@gmail.com>
2021-03-13 18:22:50 +0900

    입력된 내용을 > 텍스트뷰에 반영

--- app/src/main/java/com/neppplus/reviewkotlinebasic2_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/reviewkotlinebasic2_20210313/MainActivity.kt
@@ -11,6 +11,15 @@ class MainActivity : AppCompatActivity() {
 
         sendBtn.setOnClickListener {
 
+//            버튼이 눌리면
+//            1. inputEdt에 적힌 내용을 inputMessage 변수에 메모 : get
+
+            val inputMessage = inputEdt.text.toString()
+
+//            2. contentTxt의 text속성값을 inputMessage에 적힌 값으로 변경 : set
+
+            contentTxt.text = inputMessage
+
 
         }
 
